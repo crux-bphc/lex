c0d1d93b00807e58a0534a886d98ab522aef278c
TheComputerM <mudit.somani00@gmail.com>
2023-08-01 11:54:51 +0530

    fix: add back the course list

--- lib/modules/impartus/screens/home.dart
+++ lib/modules/impartus/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ghotpromax/modules/impartus/widgets/course.dart';
+import 'package:ghotpromax/providers/impartus.dart';
 
 class ImpartusHomePage extends StatelessWidget {
   const ImpartusHomePage({super.key});
@@ -17,7 +20,34 @@ class ImpartusHomePage extends StatelessWidget {
             )
           ],
         ),
+        const Expanded(child: _CourseList())
       ],
+    );
+  }
+}
+
+final _coursesProvider = FutureProvider((ref) {
+  final client = ref.watch(impartusClientProvider);
+  return client.fetchCourses();
+});
+
+class _CourseList extends ConsumerWidget {
+  const _CourseList();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final courses = ref.watch(_coursesProvider);
+    return courses.when(
+      data: (courses) => ListView.builder(
+        itemBuilder: (_, i) => CourseCard(course: courses[i]),
+        itemCount: courses.length,
+      ),
+      error: (error, trace) => Center(
+        child: Text("$error\n$trace"),
+      ),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
--- lib/modules/impartus/screens/lectures.dart
+++ lib/modules/impartus/screens/lectures.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 class ImpartusLecturesPage extends StatelessWidget {
@@ -21,9 +22,27 @@ class ImpartusLecturesPage extends StatelessWidget {
               context.pop();
             },
           ),
-          title: const Text("Subject Name"),
+          title: const Text("Course Name"),
         ),
+        Expanded(
+          child: _LectureList(
+            subjectId: int.parse(subjectId),
+            sessionId: int.parse(sessionId),
+          ),
+        )
       ],
     );
+  }
+}
+
+class _LectureList extends ConsumerWidget {
+  const _LectureList({required this.subjectId, required this.sessionId});
+
+  final int subjectId;
+  final int sessionId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
   }
 }
