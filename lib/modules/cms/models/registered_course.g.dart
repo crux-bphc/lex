427104d6e1399bfb60dc351f27441a19edfaa474
TheComputerM <mudit.somani00@gmail.com>
2023-08-02 23:05:30 +0530

    chore: convert to freezed

---
+++ lib/modules/cms/models/registered_course.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'registered_course.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_CMSRegisteredCourse _$$_CMSRegisteredCourseFromJson(
+        Map<String, dynamic> json) =>
+    _$_CMSRegisteredCourse(
+      id: json['id'] as int,
+      displayname: json['displayname'] as String,
+    );
