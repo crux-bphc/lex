59ea677431ddfe5162a0185ccdf2134ca0a595b4
TheComputerM <mudit.somani00@gmail.com>
2023-08-03 20:28:00 +0530

    feat: add a help dialog

--- lib/modules/settings/screens/home.dart
+++ lib/modules/settings/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:ghotpromax/modules/settings/widgets/tile.dart';
 import 'package:ghotpromax/providers/cms.dart';
 import 'package:ghotpromax/providers/impartus.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -70,6 +71,45 @@ class _ImpartusSettings extends ConsumerWidget {
 class _CMSSettings extends ConsumerWidget {
   const _CMSSettings();
 
+  Future<void> _showTokenHelpDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          insetPadding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 18,
+              vertical: 16,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "1. Login to the official CMS using your web browser",
+                ),
+                const Text("2. Go Account > Preferences > Security Keys"),
+                TextButton(
+                  onPressed: () => launchUrlString(
+                    "https://cms.bits-hyderabad.ac.in/user/managetoken.php",
+                  ),
+                  child: const Text("Manage your Security Tokens"),
+                ),
+                const Text(
+                  "3. Click on the 'Reset' button on the 'Moodle mobile web service' key",
+                ),
+                const Text(
+                  "4. Copy and paste the new 'Moodle mobile web service' key over here.",
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final token = ref.watch(cmsTokenProvider);
@@ -77,15 +117,19 @@ class _CMSSettings extends ConsumerWidget {
       title: "CMS",
       children: [
         TextFormField(
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.token),
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.token),
             labelText: "Web service token",
+            suffixIcon: IconButton(
+              onPressed: () => _showTokenHelpDialog(context),
+              icon: const Icon(Icons.help),
+            ),
           ),
           initialValue: token,
           onFieldSubmitted: (value) {
             ref.read(cmsTokenProvider.notifier).state = value;
           },
-        )
+        ),
       ],
     );
   }
