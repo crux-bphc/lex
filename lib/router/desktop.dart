ac2c5d7af5c4dabe89ee0c246a52019a1c1dee1a
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-02-19 18:32:47 +0530

    feat: replace nav rail with sidebar + tooltip

--- lib/router/desktop.dart
+++ lib/router/desktop.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:lex/router/theme_switcher.dart';
 import 'package:go_router/go_router.dart';
+
+const _navItems = [
+  _NavItem('CMS', Icons.book_outlined),
+  _NavItem('Multipartus', Icons.video_collection_outlined),
+];
 
 class DesktopScaffold extends StatelessWidget {
   const DesktopScaffold({
@@ -19,34 +23,46 @@ class DesktopScaffold extends StatelessWidget {
       body: SafeArea(
         child: Row(
           children: [
-            NavigationRail(
-              onDestinationSelected: onDestinationSelected,
-              selectedIndex: selectedIndex,
-              labelType: NavigationRailLabelType.selected,
-              destinations: const [
-                NavigationRailDestination(
-                  label: Text('CMS'),
-                  icon: Icon(Icons.library_books_outlined),
-                  selectedIcon: Icon(Icons.library_books),
-                ),
-                NavigationRailDestination(
-                  label: Text('Multipartus'),
-                  icon: Icon(Icons.smart_display_outlined),
-                  selectedIcon: Icon(Icons.smart_display),
-                ),
-              ],
-              trailing: Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
+            SizedBox(
+              width: 110,
+              child: Material(
+                elevation: 10,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      const ThemeSwitcher(),
+                      const AspectRatio(
+                        aspectRatio: 1,
+                        child: Placeholder(
+                          color: Color(0xFFEBCB8B),
+                          strokeWidth: 4,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      for (final (i, item) in _navItems.indexed)
+                        _NavButton(
+                          navItem: item,
+                          onPressed: () => onDestinationSelected(i),
+                        ),
+                      const Spacer(),
                       IconButton(
-                        icon: const Icon(Icons.settings),
-                        onPressed: () {
-                          context.go('/settings');
-                        },
+                        onPressed: () {},
+                        icon: CircleAvatar(
+                          backgroundColor: Theme.of(context).colorScheme.error,
+                          radius: 16,
+                          child: const Icon(Icons.person_outline_rounded),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      _NavToolTip(
+                        text: 'Settings',
+                        child: IconButton(
+                          icon: const Icon(Icons.settings),
+                          iconSize: 32,
+                          onPressed: () {
+                            context.go('/settings');
+                          },
+                        ),
                       ),
                     ],
                   ),
@@ -58,5 +74,151 @@ class DesktopScaffold extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _NavItem {
+  const _NavItem(this.title, this.icon);
+  final String title;
+  final IconData icon;
+}
+
+class _NavButton extends StatelessWidget {
+  const _NavButton({required this.navItem, required this.onPressed});
+
+  final _NavItem navItem;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: _NavToolTip(
+        text: navItem.title,
+        child: RawMaterialButton(
+          onPressed: onPressed,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Icon(
+            navItem.icon,
+            color: Theme.of(context).colorScheme.onBackground,
+            size: 32,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavToolTip extends StatefulWidget {
+  const _NavToolTip({
+    required this.child,
+    required this.text,
+    double? horizontalOffset,
+    Duration? duration,
+  })  : horizontalOffset = horizontalOffset ?? 50,
+        duration = duration ?? const Duration(milliseconds: 100);
+
+  @override
+  State<_NavToolTip> createState() => _NavToolTipState();
+
+  final Widget child;
+  final String text;
+  final double horizontalOffset;
+  final Duration duration;
+}
+
+class _NavToolTipState extends State<_NavToolTip>
+    with SingleTickerProviderStateMixin {
+  final _controller = OverlayPortalController();
+
+  late final _animation = AnimationController(
+    duration: widget.duration,
+    vsync: this,
+  );
+
+  late final _curvedAnimation = CurvedAnimation(
+    parent: _animation,
+    curve: Curves.easeInCubic,
+  );
+
+  Widget _buildOverlay(BuildContext context) {
+    // from Flutter Tooltip source code
+    final overlayState = Overlay.of(context);
+    final renderBox = this.context.findRenderObject()! as RenderBox;
+    final Offset target = renderBox.localToGlobal(
+      renderBox.size.center(Offset.zero),
+      ancestor: overlayState.context.findRenderObject(),
+    );
+
+    // fade and fly in animation
+    return AnimatedBuilder(
+      animation: _curvedAnimation,
+      builder: (context, child) => Positioned(
+        top: target.dy - renderBox.size.height / 2,
+        left: target.dx +
+            widget.horizontalOffset -
+            2 +
+            2 * _curvedAnimation.value,
+        child: Opacity(
+          opacity: _curvedAnimation.value,
+          child: child!,
+        ),
+      ),
+      child: SizedBox(
+        height: renderBox.size.height,
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+            decoration: BoxDecoration(
+              color: const Color(0xFFEBCB8B),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Text(
+              widget.text,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.background,
+                fontSize: 18,
+                height: 1,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _show() {
+    _controller.show();
+    _animation.forward();
+  }
+
+  void _hide() async {
+    await _animation.reverse();
+    _controller.hide();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) => _show(),
+      onExit: (event) => _hide(),
+      child: OverlayPortal(
+        controller: _controller,
+        overlayChildBuilder: _buildOverlay,
+        child: widget.child,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _curvedAnimation.dispose();
+    _animation.dispose();
+
+    super.dispose();
   }
 }
