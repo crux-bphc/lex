b7b47015a96403b6b11842d6fc2228160b319a08
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-02-20 23:53:35 +0530

    feat: add icon size param to theme switcher

--- lib/router/theme_switcher.dart
+++ lib/router/theme_switcher.dart
@@ -3,12 +3,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lex/providers/theme.dart';
 
 class ThemeSwitcher extends ConsumerWidget {
-  const ThemeSwitcher({super.key});
+  const ThemeSwitcher({
+    this.iconSize,
+    super.key,
+  });
+
+  final double? iconSize;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(themeProvider);
     return IconButton(
+      iconSize: iconSize,
       onPressed: ref.read(themeProvider.notifier).toggle,
       icon: Icon(theme == ThemeMode.light ? Icons.dark_mode : Icons.light_mode),
     );
