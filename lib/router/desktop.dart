967edcf9ba0bfe2a54cb70e673a5b6911c36ced2
TheComputerM <mudit.somani00@gmail.com>
2024-02-21 01:25:16 +0530

    refactor: use maintained lucide icons

--- lib/router/desktop.dart
+++ lib/router/desktop.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lex/router/theme_switcher.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 
 const _navItems = [
   _NavItem(
--- lib/router/theme_switcher.dart
+++ lib/router/theme_switcher.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lex/providers/theme.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 
 class ThemeSwitcher extends ConsumerWidget {
   const ThemeSwitcher({
