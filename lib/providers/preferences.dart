3c96330f1673015a4d8c4be09c239fbd9eea71da
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-02-26 01:18:42 +0530

    feat: create preferences class

--- lib/providers/preferences.dart
+++ lib/providers/preferences.dart
@@ -1,6 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:signals/signals.dart';
 
 final preferencesProvider = Provider<SharedPreferences>(
   (ref) => throw UnimplementedError(),
 );
+
+class Preferences {
+  final SharedPreferences _prefs;
+
+  late final EffectCleanup _cleanup;
+
+  Preferences(this._prefs) {
+    _cleanup = effect(() {
+      _prefs.setString('theme_mode', _themeMode.value);
+      debugPrint("set theme");
+    });
+  }
+
+  late final _themeMode = signal(_prefs.getString('theme_mode') ?? 'dark');
+  late final themeMode = computed(
+    () => _themeMode.value == 'dark' ? ThemeMode.dark : ThemeMode.light,
+  );
+
+  void toggleTheme() {
+    _themeMode.value = _themeMode.value == 'dark' ? 'light' : 'dark';
+  }
+
+  void dispose() {
+    _cleanup();
+  }
+}
