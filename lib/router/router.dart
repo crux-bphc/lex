388b3fb32370ce2e85f6153329cf3f8bb92bb768
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-06-21 21:29:13 +0400

    feat: add auth wall

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:lex/modules/cms/screens/course.dart';
 import 'package:lex/modules/cms/screens/forum.dart';
 import 'package:lex/modules/cms/screens/home.dart';
@@ -6,8 +7,10 @@ import 'package:lex/modules/cms/screens/search.dart';
 import 'package:lex/modules/cms/widgets/ensure_login.dart';
 import 'package:lex/modules/multipartus/screens/home.dart';
 import 'package:lex/modules/settings/screens/home.dart';
+import 'package:lex/providers/auth/auth_provider.dart';
 import 'package:lex/router/scaffold.dart';
 import 'package:go_router/go_router.dart';
+import 'package:signals/signals_flutter.dart';
 
 final _cmsNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'cms');
 final _multipartusNavigatorKey = GlobalKey<NavigatorState>(
@@ -63,7 +66,8 @@ final router = GoRouter(
           routes: [
             GoRoute(
               path: '/multipartus',
-              builder: (context, state) => const MultipartusHomePage(),
+              builder: (context, state) =>
+                  const AuthWall(MultipartusHomePage()),
             ),
           ],
         ),
@@ -75,3 +79,56 @@ final router = GoRouter(
     ),
   ],
 );
+
+// you shall not pass
+class AuthWall extends StatelessWidget {
+  const AuthWall(this.child, {super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = GetIt.instance<AuthProvider>();
+    return auth.isAuthed.watch(context)
+        ? child
+        : Scaffold(
+            body: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          "LEX",
+                          style: TextStyle(
+                            fontSize: 120,
+                            letterSpacing: 10,
+                            height: 1.2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          "Powered by cruX",
+                          style: TextStyle(
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        OutlinedButton(
+                          onPressed: () {
+                            auth.login();
+                          },
+                          child: const Text('LOGIN'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Expanded(flex: 1, child: SizedBox()),
+              ],
+            ),
+          );
+  }
+}
