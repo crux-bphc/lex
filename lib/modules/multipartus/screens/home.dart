f2768e4cc6e6f442785b4e937647858821d778f7
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-07-08 19:33:25 +0400

    feat: create multipartus login page

--- lib/modules/multipartus/screens/home.dart
+++ lib/modules/multipartus/screens/home.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:lex/modules/multipartus/widgets/login_gate.dart';
+import 'package:lex/modules/multipartus/widgets/multipartus_title.dart';
 
 class MultipartusHomePage extends StatelessWidget {
   const MultipartusHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Multipartus");
+    return const Scaffold(
+      body: MultipartusLoginGate(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MultipartusTitle(),
+              SizedBox(height: 20),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(flex: 5, child: _Courses()),
+                    SizedBox(width: 40),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Courses extends StatelessWidget {
+  const _Courses();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
