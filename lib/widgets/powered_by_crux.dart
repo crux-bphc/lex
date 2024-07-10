c8fcf34e3d210045713fb738b037a190e2a6429b
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2024-07-10 16:38:52 +0400

    feat: create powered by crux widget

---
+++ lib/widgets/powered_by_crux.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PoweredByCrux extends StatelessWidget {
+  const PoweredByCrux({
+    super.key,
+    required this.child,
+    this.alignment = Alignment.centerRight,
+  });
+
+  final Widget child;
+  final Alignment alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Column(
+        children: [
+          child,
+          Align(
+            alignment: alignment,
+            child: Text(
+              "Powered by cruX",
+              style: TextStyle(
+                letterSpacing: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
